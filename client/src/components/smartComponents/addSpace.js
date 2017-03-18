5f3132bc9a1a5572497a9baaaa86d319d53cc93d
taewoong <moondingie5@gmail.com>
2017-03-18 17:25:35 +0900

    Modify Space adding function

--- client/src/components/smartComponents/addSpace.js
+++ client/src/components/smartComponents/addSpace.js
@@ -1,37 +1,95 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { browserHistory } from 'react-router';
-import { API_URL } from '../../config';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+import { tokenChecker, API_URL } from '../../config';
+
+const renderMembers = ({ fields }) => (
+  <ul>
+    <button type="button" onClick={() => {
+        console.log(1);
+      const instance = {
+        headers: {
+          token: localStorage.getItem('userToken'),
+        },
+      };
+      fields.push();
+      axios({
+        method: 'get',
+        url: `${API_URL}/space`,
+        headers: instance.headers,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    }}
+    >
+      Add Information</button>
+    {fields.map((space, index) =>
+      <li key={index}>
+        <h4>Space #{index + 1}</h4>
+        <Field
+          name={`${space}.Name`}
+          type="text"
+          component="input"
+          placeholder="Name"
+        />
+        <Field
+          name={`${space}.Address`}
+          type="text"
+          component="input"
+          placeholder="Address"
+        />
+        <Field
+          name={`${space}.Max_desks`}
+          type="text"
+          component="input"
+          placeholder="Max_desks"
+        />
+        <button
+          type="button"
+          title="Remove Information"
+          onClick={() => fields.remove(index)}
+        >
+          Delete Information
+        </button>
+      </li>,
+    )}
+  </ul>
+);
 
 class AddSpace extends Component {
+
   constructor(props) {
     super(props);
     this.submitData = this.submitData.bind(this);
   }
 
   submitData(e) {
-    console.log('data', e)
-    const token = {
-      token: localStorage.getItem('userToken'),
+    const instance = {
+      headers: {
+        token: localStorage.getItem('userToken'),
+      },
     };
-    return axios({
+    const userCompanyId = localStorage.getItem('userCompanyId');
+    const data = Object.assign({}, e);
+    const spaceData = data.space[0];
+    console.log(spaceData);
+    axios({
       method: 'post',
       url: `${API_URL}/space`,
-      headers: token,
+      headers: instance.headers,
       data: {
-        company_id: e.company_id,
-        name: e.name,
-        address: e.address,
-        max_desks: e.max_desks,
+        name: spaceData.Name,
+        address: spaceData.Address,
+        max_desks: spaceData.Max_desks,
+        company_id: userCompanyId,
       },
     })
     .then((res) => {
       console.log('res', res);
-      browserHistory.push('/admin/setting/space');
-    })
-    .catch((err) => {
-      console.log('err', err);
+      browserHistory.push('/admin')
     })
   }
 
@@ -39,56 +97,18 @@ class AddSpace extends Component {
     const { handleSubmit, pristine, reset, submitting } = this.props;
     return (
       <div>
-        AddSpace
-        <br />
-        <div>
-          <form id="name" onSubmit={handleSubmit(this.submitData)}>
-            <div>
-              <label>Company Id</label>
-              <div>
-                <Field name="company_id" component="input" type="number" placeholder="Company_id" />
-              </div>
-            </div>
-            <div>
-              <label>Name</label>
-              <div>
-                <Field name="name" component="input" type="text" placeholder="Name" />
-              </div>
-            </div>
-            <div>
-              <label>Address</label>
-              <div>
-                <Field name="address" component="input" type="text" placeholder="Address" />
-              </div>
-            </div>
-            <div>
-              <label>Max Desks</label>
-              <div>
-                <Field name="max_desks" component="input" type="number" placeholder="Max_desks" />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                disabled={this.props.pristine || this.props.submitting}
-              >
-                Submit
-              </button>
-              <button
-                type="button"
-                disabled={this.props.pristine || this.props.submitting}
-                onClick={this.props.reset}
-              >
-                Clear Values
-              </button>
-            </div>
-          </form>
-        </div>
+        <form id="AddSpace" onSubmit={handleSubmit(this.submitData)}>
+          <FieldArray name="space" component={renderMembers} />
+          <div>
+            <button type="submit" disabled={submitting}>Submit</button>
+            <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
 export default reduxForm({
-  form: 'simple',
+  form: 'fieldArrays',
 })(AddSpace);
