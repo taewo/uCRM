625c3a88fa60df720bdbf7b6023f42d460d6c3aa
LeeSangHun0226 <fkdndpf1@gmail.com>
2017-03-22 13:54:21 +0900

    home background

--- client/src/components/dummyComponents/onHeader/buttonHome.js
+++ client/src/components/dummyComponents/onHeader/buttonHome.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { Button, Menu, Container } from 'semantic-ui-react';
 
-class ButtonSignUp extends Component {
+class ButtonHome extends Component {
   render() {
     return (
       <Menu.Item>
-        <Link to={'/signup'}>signup</Link>
         <Link to={'/'}>Home</Link>
       </Menu.Item>
     );
   }
 }
 
-export default ButtonSignUp;
+export default ButtonHome;
