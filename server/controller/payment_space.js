80700af743224db5a2f836666e066360b70386f4
Ilmo Koo <johnnykoo84@gmail.com>
2017-03-23 17:15:01 +0900

    unit test: /space/payment

--- server/controller/payment_space.js
+++ server/controller/payment_space.js
@@ -2,13 +2,8 @@ const paymentSpace = require('../model/payment_space');
 
 module.exports = {
   get:
-  // (req, res) => (paymentSpace.get(req))
-  (req, res) => {
-    console.log('REQ', req.query)
-    return paymentSpace.get(req)
-    .then((result) => {console.log(result)
-    return res.json(result)})
-    // .then(result => (res.json(result)))
-    .catch(err => (res.status(400).send(err)));
-  },
+  (req, res) => (paymentSpace.get(req))
+    .then(result => (res.json(result)))
+    .catch(err => (res.status(400).send(err)))
+  ,
 };
